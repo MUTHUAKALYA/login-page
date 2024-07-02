06c3144919b6f755f36ad1fc4e7f1224a6a10d38
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-07-02 13:52:54 +0530

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <button className="bg-red-800 text-xl p-5 " type="button">
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default App;
